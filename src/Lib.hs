e33c1c492173dfa4e8688626cefa68ea7f57b154
elpinal <elpinal@users.noreply.github.com>
2017-05-27 11:23:48 +0900

    Make someFunc emit assembly

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,4 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ foldl1 (\acc next -> acc ++ "\n" ++ next) ["\t.text", "\t.global mymain", "mymain:", "\tmov $42, %eax", "\tret"]
