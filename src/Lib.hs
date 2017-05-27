7d5ab73d10d4d3ed223468689864f09905bbc87d
elpinal <elpinal@users.noreply.github.com>
2017-05-27 22:43:16 +0900

    Update compile

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,77 +14,12 @@ someFunc = do
     $ putStrLn $ foldl1 (\acc next -> acc ++ "\n" ++ next) ["\t.text", "\t.global _mymain", "_mymain:", "\tmov $" ++ val ++ ", %eax", "\tret"]
 
 compile :: IO ()
-compile = do
-  c <- getChar
-  compile' c
+compile = interact compile'
 
-compile' :: Char -> IO ()
-compile' c
-  | isDigit c = compileExpr (read [c])
-  | c == '"' = compileString
-  | otherwise = error $ "unexpected character: '" ++ [c] ++ "' (" ++ (show $ ord c) ++ ")"
-
-compileExpr :: Int -> IO ()
-compileExpr n = do
-  n' <- readNumber n
-  putStrLn $ foldl1 (\acc next -> acc ++ "\n" ++ next) ["\t.text", "\t.global _intfn", "_intfn:", "\tmov $" ++ show n' ++ ", %rax"]
-  compileExpr'
-
-compileExpr' :: IO ()
-compileExpr' = do
-  c <- skipSpace
-  if isControl c then
-    putStrLn "\tret"
-  else do
-    compileOp c
-
-compileOp :: Char -> IO ()
-compileOp '+' = op "add"
-compileOp '-' = op "sub"
-compileOp c = error $ "expected operator, but got '" ++ [c] ++ "'"
-
-op :: String -> IO ()
-op s = do
-  c <- skipSpace
-  guard . isDigit $ c
-  n <- readNumber $ read [c]
-  putStrLn $ '\t' : s ++ " $" ++ show n ++ ", %rax"
-
-
-skipSpace :: IO Char
-skipSpace = do
-  c <- getChar
-  if isSpace c then
-    skipSpace
-  else
-    return c
-
-readNumber :: Int -> IO Int
-readNumber initial = do
-  x <- getChar
-  readNumber' initial x
-    where
-      readNumber' :: Int -> Char -> IO Int
-      readNumber' initial x
-        | isDigit x = readNumber (initial*10 + read [x])
-        | isSpace x = return initial
-        | isControl x = return initial
-        | otherwise = error $ "unexpected character in number: '" ++ [x] ++ "' (" ++ (show $ ord x) ++ ")"
-
-compileString :: IO ()
-compileString = do
-  s <- readString ""
-  putStrLn $ foldl1 (\acc next -> acc ++ "\n" ++ next) ["\t.data", ".mydata:", "\t.string \"" ++ s ++ "\"", "\t.text", "\t.global _stringfn", "_stringfn:", "\tlea .mydata(%rip), %rax", "\tret"]
-
-readString :: String -> IO String
-readString acc = do
-  c <- getChar
-  readString' acc c
-    where
-      readString' :: String -> Char -> IO String
-      readString' acc c
-        | c == '"' = return acc
-        | otherwise = readString $ acc ++ [c]
+compile' :: String -> String
+compile' s = case scan s of
+  Right tokens -> generate . parse' $ tokens
+  Left err -> error . show $ err
 
 
 
