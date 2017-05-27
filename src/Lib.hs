e2ba687f172b96e2b8024025049420ca47ae6b5c
elpinal <elpinal@users.noreply.github.com>
2017-05-27 11:41:01 +0900

    Enable someFunc to take a number from standard input

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,11 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Char
+import Control.Monad
+
 someFunc :: IO ()
-someFunc = putStrLn $ foldl1 (\acc next -> acc ++ "\n" ++ next) ["\t.text", "\t.global mymain", "mymain:", "\tmov $42, %eax", "\tret"]
+someFunc = do
+  val <- getLine
+  when ((not $ null val) && (all isDigit val))
+    $ putStrLn $ foldl1 (\acc next -> acc ++ "\n" ++ next) ["\t.text", "\t.global mymain", "mymain:", "\tmov $" ++ val ++ ", %eax", "\tret"]
