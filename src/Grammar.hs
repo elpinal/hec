ca4f5fec3d4b8a99c0e009e0ffb89f069ed65d38
elpinal <elpinal@users.noreply.github.com>
2017-05-30 22:37:14 +0900

    Add first function

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -21,6 +21,33 @@ data Term =
 
 data SymbolSet = SymbolSet [Symbol] deriving (Show)
 
+first :: [Grammar] -> [(Symbol, [Symbol])]
+first [] = []
+first (x:xs) = nub $ (getHead x, first' nullable x) : first xs
+  where nullable = (`elem` (nulls (x:xs)))
+
+first' :: (Symbol -> Bool) -> Grammar -> [Symbol]
+first' f (Grammar _ (SymbolSet [])) = []
+first' f (Grammar _ (SymbolSet (x@(Term _):_))) = [x]
+first' f (Grammar head (SymbolSet body)) =
+  let
+    ys = takeUpToNot f body
+  in
+    filter isTerm ys
+  where
+    isTerm :: Symbol -> Bool
+    isTerm (Term _) = True
+    isTerm _ = False
+
+takeUpToNot :: (a -> Bool) -> [a] -> [a]
+takeUpToNot f xs =
+  let
+    (l, r) = span f xs
+  in
+    case r of
+      [] -> l
+      otherwise -> l ++ [head r]
+
 nulls :: [Grammar] -> [Symbol]
 nulls [] = []
 nulls gs =
