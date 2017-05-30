9aa28c891b4e9a882fb6edd97b50f588f69afe88
elpinal <elpinal@users.noreply.github.com>
2017-05-30 19:30:43 +0900

    Support Grammar representation

---
+++ src/Grammar.hs
@@ -0,0 +1,48 @@
+module Grammar
+    (
+    ) where
+
+data Grammar = Grammar Symbol SymbolSet
+
+data Term =
+    Num
+  | Ident
+    deriving (Eq, Show)
+
+data Symbol =
+    Term Term
+  | NonTerm String
+    deriving (Eq, Show)
+
+data SymbolSet = SymbolSet [Symbol]
+
+justNull :: Grammar -> Bool
+justNull (Grammar _ (SymbolSet [])) = True
+justNull _ = False
+
+nulls :: [Grammar] -> [Symbol]
+nulls [] = []
+nulls gs =
+  let
+    bits = zip (map justNull gs) gs
+    tg = map ((\(Grammar head _) -> head) . snd) $ filter fst bits
+    fg = map snd $ filter (not . fst) bits
+  in
+    converge (nulls' fg) tg
+
+nulls' :: [Grammar] -> [Symbol] -> [Symbol]
+nulls' [] ns = ns
+nulls' ((Grammar x (SymbolSet ss)):xs) ns
+  | all (\x -> willBeNull x ns) ss = nulls' xs (x:ns)
+  | otherwise = nulls' xs ns
+
+willBeNull :: Symbol -> [Symbol] -> Bool
+willBeNull (Term _) xs = False
+willBeNull sym xs = sym `elem` xs
+
+converge :: Eq a => (a -> a) -> a -> a
+converge f y = let r = f y in
+  if r == y then
+    r
+  else
+    converge f r
