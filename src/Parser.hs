7d3dbdf70cc1c4d0bdd18fefa0fad7e120993c10
elpinal <elpinal@users.noreply.github.com>
2017-06-02 14:55:04 +0900

    Add skeltons of action & goto

--- src/Parser.hs
+++ src/Parser.hs
@@ -16,7 +16,28 @@ data Symbol = Term Term | NonTerm NonTerm
 
 data AST = Node [AST] | Leaf Token
 
+data State = State Int
+
+data Action =
+    Shift Int
+  | Reduce Rule
+  | Accept
+
 extend :: Grammar -> Grammar
 extend (Grammar start rules) = Grammar Start ((Rule Start [NonTerm start]):rules)
 
 parse :: Grammar -> [Token] -> AST
+parse _ [] = Node []
+parse grammar tokens =
+  let
+    f = action grammar
+    g = goto grammar
+    steps = parse' f g tokens
+  in
+    makeAST steps tokens
+
+action :: Grammar -> State -> Token -> Action
+goto :: Grammar -> String -> Token -> Int
+
+makeAST :: [Rule] -> [Token] -> AST
+makeAST steps tokens = Node []
