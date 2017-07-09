0b6a1767db79f8aa486ad43a754355d28782a39c
elpinal <elpinal@users.noreply.github.com>
2017-07-09 09:28:14 +0900

    Follow HLint

--- src/Gen.hs
+++ src/Gen.hs
@@ -37,7 +37,7 @@ generate :: Block -> (Seq Code, Register)
 generate block = uncurry (gen . viewr) (checkLabel block) $ Set.fromList [EAX, EBX, ECX, EDX]
 
 gen :: ViewR Inter.Quad -> Map.Map Inter.Addr Int -> Set.Set Register -> (Seq Code, Register)
-gen (xs:>(res, op, Inter.At addr1, Inter.At addr2)) m registers =
+gen (xs:>(_, op, Inter.At addr1, Inter.At addr2)) m registers =
   if fromMaybe (error "unexpected error") $ Map.lookup addr1 m .> Map.lookup addr2 m then
     case viewr $ dropWhileR (\(Inter.Point addr, _, _, _) -> addr /= addr1) xs of
     Sequence.EmptyR -> error $ "unexpected error: no such address: " ++ show addr1
@@ -61,22 +61,22 @@ gen (xs:>(res, op, Inter.At addr1, Inter.At addr2)) m registers =
                         (codes2, reg2) = gen pre m $ Set.delete reg1 registers
                       in ((codes1 >< codes2) |> Code (instr op) [Reg reg1, Reg reg2], reg1)
   where (.>) = liftA2 (>)
-gen (xs:>(res, op, Inter.Const v1, Inter.Const v2)) m registers =
+gen (_:>(_, op, Inter.Const v1, Inter.Const v2)) _ registers =
   let reg = Set.findMin registers
     in (Sequence.fromList [Code Mov [Const v1, Reg reg], Code (instr op) [Reg reg, Const v2]], reg)
-gen (xs:>(res, op, Inter.At addr, Inter.Const v)) m registers =
+gen (xs:>(_, op, Inter.At addr, Inter.Const v)) m registers =
   case viewr $ dropWhileR (\(Inter.Point a, _, _, _) -> a /= addr) xs of
   Sequence.EmptyR -> error $ "unexpected error: no such address: " ++ show addr
   pre -> let (codes, reg) = gen pre m registers
     in (codes |> Code (instr op) [Reg reg, Const v], reg)
-gen (xs:>(res, op, Inter.Const v, Inter.At addr)) m registers =
+gen (xs:>(_, op, Inter.Const v, Inter.At addr)) m registers =
   case viewr $ dropWhileR (\(Inter.Point a, _, _, _) -> a /= addr) xs of
   Sequence.EmptyR -> error $ "unexpected error: no such address: " ++ show addr
   pre -> let
     (codes, reg) = gen pre m registers
     reg1 = Set.findMin registers
     in (codes |> Code Mov [Const v, Reg reg1] |> Code (instr op) [Reg reg1, Reg reg], reg)
-gen (xs:>(res, Inter.NOP, Inter.Const v, Inter.Nil)) m registers =
+gen (_:>(_, Inter.NOP, Inter.Const v, Inter.Nil)) _ registers =
   let reg = Set.findMin registers
     in (singleton $ Code Mov [Const v, Reg reg], reg)
 
@@ -93,6 +93,6 @@ label (_, _, operand1, Inter.Nil) m = labelOf operand1 m
 label (_, _, operand1, operand2) m = 1 + min (labelOf operand1 m) (labelOf operand2 m)
 
 labelOf :: Inter.Operand -> Map.Map Inter.Addr Int -> Int
-labelOf (Inter.Const v) _ = 0
+labelOf (Inter.Const _) _ = 0
 labelOf (Inter.At addr) m = fromMaybe (error $ "no such address in the block: " ++ show addr) $ Map.lookup addr m
 labelOf Inter.Nil _ = error "unexpected operand: nil"
