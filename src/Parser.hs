f562a65bcf4cd09ede226d62063a512298ecf261
elpinal <elpinal@users.noreply.github.com>
2017-09-24 22:42:07 +0900

    Use headMay in Safe module
    headMaybe was the same as headMay.

--- src/Parser.hs
+++ src/Parser.hs
@@ -136,11 +136,7 @@ action' gotoF states current token
     fromToken' (Token' (Token (term, _))) = Term term
 
 getID :: Map.Map Int Items -> Items -> Maybe Int
-getID states items = headMaybe . Map.keys . Map.filter (==items) $ states
-  where
-    headMaybe :: [a] -> Maybe a
-    headMaybe [] = Nothing
-    headMaybe (x:_) = Just x
+getID states items = headMay . Map.keys . Map.filter (==items) $ states
 
 eqLaToken :: LookAhead -> Token' -> Bool
 eqLaToken EndPoint EndToken = True
