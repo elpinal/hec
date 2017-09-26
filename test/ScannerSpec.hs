53ce54430f88b456b1643259d42b3d3a84e9d7ae
elpinal <elpinal@users.noreply.github.com>
2017-09-26 21:35:12 +0900

    Update tests for Scanner

--- test/ScannerSpec.hs
+++ test/ScannerSpec.hs
@@ -6,8 +6,14 @@ import Scanner
 
 spec :: Spec
 spec = do
-  describe "Token1" $
+  describe "Token1" $ do
     it "implements Functor" $ do
       (+ 12) <$> Token1 "lit" 7
       `shouldBe`
       Token1 "lit" 19
+    it "follows Functor's laws" $ do
+      let token = Token1 "lit" 2
+      fmap id token `shouldBe` id token
+      let f = (* 3)
+          g = (+ 12)
+      fmap (f . g) token `shouldBe` (fmap f . fmap g) token
