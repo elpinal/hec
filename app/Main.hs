4bb9500ddf217738d5818ef9dfa25a16bf093d3c
elpinal <elpinal@users.noreply.github.com>
2017-09-27 11:15:50 +0900

    Exit with a proper code

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Control.Monad
+import System.Exit
 import System.IO
 
 import Compile
 
 main :: IO ()
-main = getContents >>= either (hPutStr stderr) putStr . compile
+main = getContents >>= either (hPutStr stderr >=> const exitFailure) putStr . compile
