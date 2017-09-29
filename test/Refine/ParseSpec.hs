c638be56d12e6f48e57bae515b9c5e5af644db88
elpinal <elpinal@users.noreply.github.com>
2017-09-29 23:14:45 +0900

    Simplify test

--- test/Refine/ParseSpec.hs
+++ test/Refine/ParseSpec.hs
@@ -12,7 +12,7 @@ spec :: Spec
 spec =
   describe "parseExpr" $ do
     it "parses an expression" $ do
-      (parseExpr "1" :: Either ParseError (Expr Int)) `shouldSatisfy` const False ||| (== Num 1)
-      (parseExpr "succ1" :: Either ParseError (Expr Int)) `shouldSatisfy` const False ||| (== Succ (Num 1))
-      (parseExpr "toIntFalse" :: Either ParseError (Expr Int)) `shouldSatisfy` const False ||| (== ToInt (Bool False))
-      (parseExpr "succtoIntTrue" :: Either ParseError (Expr Int)) `shouldSatisfy` const False ||| (== Succ (ToInt (Bool True)))
+      parseExpr "1" `shouldSatisfy` const False ||| (== Num 1)
+      parseExpr "succ1" `shouldSatisfy` const False ||| (== Succ (Num 1))
+      parseExpr "toIntFalse" `shouldSatisfy` const False ||| (== ToInt (Bool False))
+      parseExpr "succtoIntTrue" `shouldSatisfy` const False ||| (== Succ (ToInt (Bool True)))
