0e7d86414120fa8d5ec29842b2c67589a6d1885b
elpinal <elpinal@users.noreply.github.com>
2017-10-10 22:56:49 +0900

    Align tests

--- test/Refine/ParseSpec.hs
+++ test/Refine/ParseSpec.hs
@@ -20,32 +20,32 @@ spec :: Spec
 spec =
   describe "parseExpr" $ do
     it "parses an expression" $ do
-      parseExpr "1" `shouldSatisfy` rightIs (int 1)
-      parseExpr "(1)" `shouldSatisfy` rightIs (int 1)
-      parseExpr "( 1 )" `shouldSatisfy` rightIs (int 1)
-      parseExpr "x" `shouldSatisfy` rightIs (Var "x")
-      parseExpr "'c'" `shouldSatisfy` rightIs (Lit (LitChar 'c'))
-      parseExpr "'\\''" `shouldSatisfy` rightIs (Lit (LitChar '\''))
-      parseExpr "\"\"" `shouldSatisfy` rightIs (Lit (LitString ""))
-      parseExpr "\"aaa\"" `shouldSatisfy` rightIs (Lit (LitString "aaa"))
-      parseExpr "\"\\\"\"" `shouldSatisfy` rightIs (Lit (LitString "\""))
-      parseExpr "x 1" `shouldSatisfy` rightIs (App (Var "x") (int 1))
-      parseExpr "f 1" `shouldSatisfy` rightIs (App (Var "f") (int 1))
-      parseExpr "(f 1)" `shouldSatisfy` rightIs (App (Var "f") (int 1))
-      parseExpr "( f 1 )" `shouldSatisfy` rightIs (App (Var "f") (int 1))
-      parseExpr "g False" `shouldSatisfy` rightIs (App (Var "g") (bool False))
-      parseExpr "f (g True)" `shouldSatisfy` rightIs (App (Var "f") (App (Var "g") (bool True)))
+      parseExpr "1"            `shouldSatisfy` rightIs (int 1)
+      parseExpr "(1)"          `shouldSatisfy` rightIs (int 1)
+      parseExpr "( 1 )"        `shouldSatisfy` rightIs (int 1)
+      parseExpr "x"            `shouldSatisfy` rightIs (Var "x")
+      parseExpr "'c'"          `shouldSatisfy` rightIs (Lit (LitChar 'c'))
+      parseExpr "'\\''"        `shouldSatisfy` rightIs (Lit (LitChar '\''))
+      parseExpr "\"\""         `shouldSatisfy` rightIs (Lit (LitString ""))
+      parseExpr "\"aaa\""      `shouldSatisfy` rightIs (Lit (LitString "aaa"))
+      parseExpr "\"\\\"\""     `shouldSatisfy` rightIs (Lit (LitString "\""))
+      parseExpr "x 1"          `shouldSatisfy` rightIs (App (Var "x") (int 1))
+      parseExpr "f 1"          `shouldSatisfy` rightIs (App (Var "f") (int 1))
+      parseExpr "(f 1)"        `shouldSatisfy` rightIs (App (Var "f") (int 1))
+      parseExpr "( f 1 )"      `shouldSatisfy` rightIs (App (Var "f") (int 1))
+      parseExpr "g False"      `shouldSatisfy` rightIs (App (Var "g") (bool False))
+      parseExpr "f (g True)"   `shouldSatisfy` rightIs (App (Var "f") (App (Var "g") (bool True)))
       parseExpr "f ( g True )" `shouldSatisfy` rightIs (App (Var "f") (App (Var "g") (bool True)))
 
     it "parses binary operations" $ do
-      parseExpr "1#2" `shouldSatisfy` rightIs (BinOp "#" (int 1) (int 2))
-      parseExpr "1 # 2" `shouldSatisfy` rightIs (BinOp "#" (int 1) (int 2))
+      parseExpr "1#2"     `shouldSatisfy` rightIs (BinOp "#" (int 1) (int 2))
+      parseExpr "1 # 2"   `shouldSatisfy` rightIs (BinOp "#" (int 1) (int 2))
       parseExpr "1 #$! 2" `shouldSatisfy` rightIs (BinOp "#$!" (int 1) (int 2))
 
-      parseExpr "1 # 2 ! 3" `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
-      parseExpr "(1 # 2 ! 3)" `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
-      parseExpr "( 1 # 2 ! 3 )" `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
-      parseExpr "((1 # 2 ! 3))" `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
+      parseExpr "1 # 2 ! 3"       `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
+      parseExpr "(1 # 2 ! 3)"     `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
+      parseExpr "( 1 # 2 ! 3 )"   `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
+      parseExpr "((1 # 2 ! 3))"   `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
       parseExpr "( (1 # 2 ! 3) )" `shouldSatisfy` rightIs (BinOp "!" (BinOp "#" (int 1) (int 2)) (int 3))
 
       let want = BinOp "##" (BinOp "!" (BinOp "#" (int 1) (App (Var "f") (int 2))) $ App (Var "f") $ App (Var "g") $ bool True) (int 3)
@@ -55,5 +55,5 @@ spec =
       parseExpr "1 # (2 ! 3)" `shouldSatisfy` rightIs (BinOp "#" (int 1) (BinOp "!" (int 2) (int 3)))
 
     it "returns an error when extra tokens appear" $ do
-      parseExpr "1f" `shouldSatisfy` isLeft
+      parseExpr "1f"    `shouldSatisfy` isLeft
       parseExpr "TrueA" `shouldSatisfy` isLeft
