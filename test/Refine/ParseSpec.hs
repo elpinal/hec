c86f5f9eac9ca8b22cec8e99e930f1d92a232dca
elpinal <elpinal@users.noreply.github.com>
2017-10-10 20:31:28 +0900

    Update test for Refine.Parser

--- test/Refine/ParseSpec.hs
+++ test/Refine/ParseSpec.hs
@@ -12,7 +12,9 @@ spec =
   describe "parseExpr" $ do
     it "parses an expression" $ do
       parseExpr "1" `shouldSatisfy` const False ||| (== Num 1)
+      parseExpr "(1)" `shouldSatisfy` const False ||| (== Num 1)
       parseExpr "succ 1" `shouldSatisfy` const False ||| (== App Succ (Num 1))
+      parseExpr "(succ 1)" `shouldSatisfy` const False ||| (== App Succ (Num 1))
       parseExpr "toInt False" `shouldSatisfy` const False ||| (== App ToInt (Bool False))
       parseExpr "succ (toInt True)" `shouldSatisfy` const False ||| (== App Succ (App ToInt (Bool True)))
 
@@ -22,6 +24,8 @@ spec =
       parseExpr "1 #$! 2" `shouldSatisfy` const False ||| (== BinOp "#$!" (Num 1) (Num 2))
 
       parseExpr "1 # 2 ! 3" `shouldSatisfy` const False ||| (== BinOp "!" (BinOp "#" (Num 1) (Num 2)) (Num 3))
+      parseExpr "(1 # 2 ! 3)" `shouldSatisfy` const False ||| (== BinOp "!" (BinOp "#" (Num 1) (Num 2)) (Num 3))
+      parseExpr "((1 # 2 ! 3))" `shouldSatisfy` const False ||| (== BinOp "!" (BinOp "#" (Num 1) (Num 2)) (Num 3))
 
       let want = BinOp "##" (BinOp "!" (BinOp "#" (Num 1) (App Succ (Num 2))) $ App Succ $ App ToInt $ Bool True) (Num 3)
       parseExpr "1 # succ 2 ! succ (toInt True) ## 3" `shouldSatisfy` const False ||| (== want)
