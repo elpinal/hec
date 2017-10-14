b0d20e37883784cf0fbbe44f0ead70a81c78a796
elpinal <elpinal@users.noreply.github.com>
2017-10-14 23:22:46 +0900

    Use WriterT to store instructions of assembly

--- src/Refine/Gen.hs
+++ src/Refine/Gen.hs
@@ -1,20 +1,25 @@
 module Refine.Gen where
 
 import Control.Monad.Reader
+import Control.Monad.Writer.Lazy
 
 import qualified Refine.Asm as Asm
 import Refine.Inter
 
-type Machine = Reader Int
+type Machine = WriterT [Asm.Inst] (Reader Int)
 
 -- | Runs a @Machine@ with the number of regesters.
-runMachine :: Machine a -> Int -> a
-runMachine m n = runReader m n
+runMachine :: Machine a -> Int -> (a, [Asm.Inst])
+runMachine m n = runReader (runWriterT m) n
 
-gen :: Address -> [ThreeAddress] -> Machine [Asm.Inst]
-gen (Const c) _ = return [genConst c]
+gen :: Address -> [ThreeAddress] -> Machine Asm.Operand
+gen (Const c) _ = return $ genConst c
+gen (Name s) xs = mapM_ gen' xs >> return (Asm.LValue s)
 gen _ _ = undefined
 
-genConst :: Constant -> Asm.Inst
-genConst (CInt c) = Asm.Ret . Asm.Const $ Asm.CInt c
-genConst (CBool b) = Asm.Ret . Asm.Const $ Asm.CBool b
+genConst :: Constant -> Asm.Operand
+genConst (CInt c) = Asm.Const $ Asm.CInt c
+genConst (CBool b) = Asm.Const $ Asm.CBool b
+
+gen' :: ThreeAddress -> Machine ()
+gen' = undefined
--- test/Refine/GenSpec.hs
+++ test/Refine/GenSpec.hs
@@ -10,10 +10,10 @@ spec :: Spec
 spec = do
   describe "gen" $ do
     it "generates Elacht assembly code from 3 address code" $ do
-      runMachine (gen (Const $ CInt 3) []) 1 `shouldBe` [Asm.Ret . Asm.Const . Asm.CInt $ 3]
+      runMachine (gen (Const $ CInt 3) []) 1 `shouldBe` (Asm.Const . Asm.CInt $ 3, [])
 
       runMachine (gen (Name "x") [BinAssign (Name "x") (Bin "-") (Name "y") (Name "z")]) 2
         `shouldBe`
-        [ Asm.ISub (Asm.Register 0) (Asm.Reg $ Asm.Register 0) (Asm.Reg $ Asm.Register 1)
-        , Asm.Ret . Asm.Reg . Asm.Register $ 0
-        ]
+        ( Asm.Reg . Asm.Register $ 0,
+          [Asm.ISub (Asm.Register 0) (Asm.Reg $ Asm.Register 0) (Asm.Reg $ Asm.Register 1)]
+        )
