0ddf0a473fd53ec425bf96a847503993183c21c0
elpinal <elpinal@users.noreply.github.com>
2017-10-17 21:40:35 +0900

    Add Type1

--- src/Refine/Type.hs
+++ src/Refine/Type.hs
@@ -1,5 +1,7 @@
 module Refine.Type where
 
+import Refine.Kind
+
 data Type =
     TypeInt
   | TypeBool
@@ -8,3 +10,22 @@ data Type =
   | TypeFun Type Type
   | TypeVar String
   deriving (Eq, Show)
+
+data Type1 =
+    TypeVar1 TVar
+  | TypeApp Type1 Type1
+  | TypeCon TCon
+
+data TVar = TVar String Kind
+
+data TCon = TCon String Kind
+
+tBool, tChar, tInt, tList, tArrow, tTuple2 :: Type1
+
+tBool = TypeCon $ TCon "Bool" Star
+tChar = TypeCon $ TCon "Char" Star
+tInt = TypeCon $ TCon "Int" Star
+
+tList = TypeCon . TCon "[]" $ KFun Star Star
+tArrow = TypeCon . TCon "(->)" . KFun Star $ KFun Star Star
+tTuple2 = TypeCon . TCon "(,)" . KFun Star $ KFun Star Star
