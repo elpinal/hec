9d02b70762867e3f99a385ac779e47621595dcba
elpinal <elpinal@users.noreply.github.com>
2017-10-22 22:53:51 +0900

    Add 'candidates'

--- src/Refine/Type.hs
+++ src/Refine/Type.hs
@@ -415,3 +415,10 @@ type Ambiguity = (TVar, [Pred])
 
 ambiguities :: ClassEnv -> Set.Set TVar -> [Pred] -> [Ambiguity]
 ambiguities ce vs ps = [(v, filter (elem v . ftv) ps) | v <- Set.toList $ ftv ps Set.\\ vs]
+
+candidates :: ClassEnv -> Ambiguity -> [Type1]
+candidates ce (v, qs) = [t' | let is = [i | IsIn i t <- qs]
+                                  ts = [t | IsIn i t <- qs],
+                              all ((TypeVar1 v) ==) ts,
+                              t' <- defaults ce,
+                              all (entail ce []) [IsIn i t' | i <- is]]
