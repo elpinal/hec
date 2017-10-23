61b0c2800b2a64eb495979e11de6bfc09e7282c7
elpinal <elpinal@users.noreply.github.com>
2017-10-23 23:54:21 +0900

    Add test for @@

--- test/Refine/TypeSpec.hs
+++ test/Refine/TypeSpec.hs
@@ -8,7 +8,7 @@ import Refine.Kind
 import Refine.Type
 
 spec :: Spec
-spec =
+spec = do
   describe "apply" $
     it "substitutes type variables in types" $ do
       Map.empty                             `apply` tInt                       `shouldBe` tInt
@@ -18,3 +18,24 @@ spec =
 
       Map.empty                             `apply` TypeApp (TypeVar1 (TVar "aaa" Star)) (TypeVar1 (TVar "bbb" Star)) `shouldBe` TypeApp (TypeVar1 (TVar "aaa" Star)) (TypeVar1 (TVar "bbb" Star))
       Map.singleton (TVar "aaa" Star) tBool `apply` TypeApp (TypeVar1 (TVar "aaa" Star)) (TypeVar1 (TVar "bbb" Star)) `shouldBe` TypeApp tBool (TypeVar1 (TVar "bbb" Star))
+
+  describe "@@" $ do
+    it "composes two substitutions" $ do
+      Map.empty @@ Map.empty `shouldBe` Map.empty
+
+      let aToInt = Map.fromList [(TVar "a" Star, tInt)]
+
+      Map.empty @@ aToInt `shouldBe` aToInt
+      aToInt @@ Map.empty `shouldBe` aToInt
+      aToInt @@ aToInt    `shouldBe` aToInt
+
+      let bToInt = Map.fromList [(TVar "b" Star, tInt)]
+
+      aToInt @@ bToInt `shouldBe` Map.fromList [(TVar "a" Star, tInt), (TVar "b" Star, tInt)]
+      bToInt @@ aToInt `shouldBe` Map.fromList [(TVar "a" Star, tInt), (TVar "b" Star, tInt)]
+
+    it "gives priority to the second argument" $ do
+      let aToInt = Map.fromList [(TVar "a" Star, tInt)]
+          aToChar = Map.fromList [(TVar "a" Star, tChar)]
+
+      aToInt @@ aToChar `shouldBe` aToChar
