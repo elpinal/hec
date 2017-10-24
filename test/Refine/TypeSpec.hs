1ba56fa9c11ddfb4f8b9ec63cc6bd8735e011474
elpinal <elpinal@users.noreply.github.com>
2017-10-24 18:33:40 +0900

    Extract the definitions of two substitutions, a -> Int and b -> Int

--- test/Refine/TypeSpec.hs
+++ test/Refine/TypeSpec.hs
@@ -23,25 +23,19 @@ spec = do
     it "composes two substitutions" $ do
       Map.empty @@ Map.empty `shouldBe` Map.empty
 
-      let aToInt = Map.fromList [(TVar "a" Star, tInt)]
-
       Map.empty @@ aToInt `shouldBe` aToInt
       aToInt @@ Map.empty `shouldBe` aToInt
       aToInt @@ aToInt    `shouldBe` aToInt
-
-      let bToInt = Map.fromList [(TVar "b" Star, tInt)]
 
       aToInt @@ bToInt `shouldBe` Map.fromList [(TVar "a" Star, tInt), (TVar "b" Star, tInt)]
       bToInt @@ aToInt `shouldBe` Map.fromList [(TVar "a" Star, tInt), (TVar "b" Star, tInt)]
 
     it "gives priority to the second argument" $ do
-      let aToInt = Map.fromList [(TVar "a" Star, tInt)]
-          aToChar = Map.fromList [(TVar "a" Star, tChar)]
+      let aToChar = Map.fromList [(TVar "a" Star, tChar)]
 
       aToInt @@ aToChar `shouldBe` aToChar
 
       let aToB = Map.singleton (TVar "a" Star) (TypeVar1 $ TVar "b" Star)
-          bToInt = Map.singleton (TVar "b" Star) tInt
 
       bToInt @@ aToB `shouldBe` Map.union aToInt bToInt
       aToB @@ bToInt `shouldBe` Map.union aToB bToInt
@@ -50,27 +44,26 @@ spec = do
     it "composes two substitutions parallel" $ do
       Map.empty `merge` Map.empty `shouldBe` Just Map.empty
 
-      let aToInt = Map.fromList [(TVar "a" Star, tInt)]
-
       Map.empty `merge` aToInt `shouldBe` Just aToInt
       aToInt `merge` Map.empty `shouldBe` Just aToInt
       aToInt `merge` aToInt    `shouldBe` Just aToInt
-
-      let bToInt = Map.fromList [(TVar "b" Star, tInt)]
 
       aToInt `merge` bToInt `shouldBe` (Just . Map.fromList) [(TVar "a" Star, tInt), (TVar "b" Star, tInt)]
       bToInt `merge` aToInt `shouldBe` (Just . Map.fromList) [(TVar "a" Star, tInt), (TVar "b" Star, tInt)]
 
     it "fails when one or more mappings conflict" $ do
-      let aToInt = Map.fromList [(TVar "a" Star, tInt)]
-          aToChar = Map.fromList [(TVar "a" Star, tChar)]
+      let aToChar = Map.fromList [(TVar "a" Star, tChar)]
 
       aToInt `merge` aToChar `shouldBe` Nothing
 
     it "is symmetric if success" $ do
       let aToB = Map.singleton (TVar "a" Star) (TypeVar1 $ TVar "b" Star)
-          aToInt = Map.fromList [(TVar "a" Star, tInt)]
-          bToInt = Map.singleton (TVar "b" Star) tInt
 
       bToInt `merge` aToB `shouldBe` Just (Map.union bToInt aToB)
       aToB `merge` bToInt `shouldBe` Just (Map.union bToInt aToB)
+
+aToInt :: Subst
+aToInt = Map.fromList [(TVar "a" Star, tInt)]
+
+bToInt :: Subst
+bToInt = Map.fromList [(TVar "b" Star, tInt)]
