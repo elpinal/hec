91b55b84a8c6a6393aa804d274a434917e2cea6f
elpinal <elpinal@users.noreply.github.com>
2017-10-25 23:50:19 +0900

    Update encodeConstAs64

--- src/Refine/Asm/Amd64.hs
+++ src/Refine/Asm/Amd64.hs
@@ -52,3 +52,6 @@ encode (Load r (Const c)) = B.pack [rex .|. rexW, 0xb8 + runRegister r] `B.appen
 
 encodeConstAs64 :: Constant -> B.ByteString
 encodeConstAs64 (CInt8 n) = B.pack (replicate 7 0x00) `B.snoc` fromIntegral n
+encodeConstAs64 (CInt16 n) = B.pack (replicate 6 0x00) `B.append` (B.pack . map fromIntegral) [n .&. 0x00ff, n .&. 0xff00]
+encodeConstAs64 (CInt32 n) = B.pack (replicate 4 0x00) `B.append` (B.pack . map fromIntegral) [n .&. 0x000000ff, n .&. 0x0000ff00, n .&. 0x00ff0000, n .&. 0xff000000]
+encodeConstAs64 (CInt64 n) = B.pack $ map fromIntegral [n .&. 0x000000ff, n .&. 0x0000ff00, n .&. 0x00ff0000, n .&. 0xff000000]
