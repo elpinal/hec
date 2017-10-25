6fc49e403f10e3187f25315aacfe5decef51a566
elpinal <elpinal@users.noreply.github.com>
2017-10-25 23:03:31 +0900

    Implement Enum for GPR

--- src/Refine/Asm/Amd64.hs
+++ src/Refine/Asm/Amd64.hs
@@ -13,3 +13,22 @@ data GPR =
   | RBP
   | RSP
   -- R8 .. R15
+
+instance Enum GPR where
+  fromEnum RAX = 0x00
+  fromEnum RBX = 0x03
+  fromEnum RCX = 0x01
+  fromEnum RDX = 0x02
+  fromEnum RDI = 0x07
+  fromEnum RSI = 0x06
+  fromEnum RBP = 0x05
+  fromEnum RSP = 0x04
+
+  toEnum 0x00 = RAX
+  toEnum 0x03 = RBX
+  toEnum 0x01 = RCX
+  toEnum 0x02 = RDX
+  toEnum 0x07 = RDI
+  toEnum 0x06 = RSI
+  toEnum 0x05 = RBP
+  toEnum 0x04 = RSP
