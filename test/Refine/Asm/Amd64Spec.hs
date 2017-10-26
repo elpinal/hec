83400c5a354889260f1ec97a063b2d189477d048
elpinal <elpinal@users.noreply.github.com>
2017-10-26 23:32:26 +0900

    Update test

--- test/Refine/Asm/Amd64Spec.hs
+++ test/Refine/Asm/Amd64Spec.hs
@@ -22,3 +22,5 @@ spec = do
 
       encode (Load (Register 0) . Const $ CInt8 (-1)) `shouldBe` B.pack [0x48, 0xb8, 255, 0, 0, 0, 0, 0, 0, 0]
       encode (Load (Register 0) . Const $ CInt8 127) `shouldBe` B.pack [0x48, 0xb8, 127, 0, 0, 0, 0, 0, 0, 0]
+
+      encode (Load (Register 0) . Const $ CInt16 32767) `shouldBe` B.pack [0x48, 0xb8, 0xff, 0x7f, 0, 0, 0, 0, 0, 0]
