d0f2a3888c13a9303061b15fa72551efd11466fa
elpinal <elpinal@users.noreply.github.com>
2017-10-26 23:02:58 +0900

    Update test

--- test/Refine/Asm/Amd64Spec.hs
+++ test/Refine/Asm/Amd64Spec.hs
@@ -11,7 +11,7 @@ spec :: Spec
 spec = do
   describe "encode" $
     it "encodes an assembly instruction to amd64 machine code" $ do
-      let load9toRAX = B.pack [0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0x09]
+      let load9toRAX = B.pack [0x48, 0xb8, 0x09, 0, 0, 0, 0, 0, 0, 0]
 
       encode (Load (Register 0)              . Const $ CInt8 9) `shouldBe` load9toRAX
       encode (Load (Register $ fromEnum RAX) . Const $ CInt8 9) `shouldBe` load9toRAX
