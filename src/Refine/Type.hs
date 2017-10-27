f72259836ba64b6825d407e24fd2f3f156e62679
elpinal <elpinal@users.noreply.github.com>
2017-10-27 22:27:26 +0900

    Rename Type1 -> Type

--- src/Refine/Type.hs
+++ src/Refine/Type.hs
@@ -12,11 +12,9 @@ import qualified Data.Set as Set
 import Refine.AST
 import Refine.Kind
 
-type Type = Type1
-
-data Type1 =
+data Type =
     TypeVar TVar
-  | TypeApp Type1 Type1
+  | TypeApp Type Type
   | TypeCon TCon
   | TypeGen Int
   deriving (Eq, Show)
@@ -27,7 +25,7 @@ data TVar = TVar String Kind
 data TCon = TCon String Kind
   deriving (Eq, Show)
 
-tBool, tChar, tInt, tString, tList, tArrow, tTuple2 :: Type1
+tBool, tChar, tInt, tString, tList, tArrow, tTuple2 :: Type
 
 tBool = TypeCon $ TCon "Bool" Star
 tChar = TypeCon $ TCon "Char" Star
@@ -39,13 +37,13 @@ tList = TypeCon . TCon "[]" $ KFun Star Star
 tArrow = TypeCon . TCon "(->)" . KFun Star $ KFun Star Star
 tTuple2 = TypeCon . TCon "(,)" . KFun Star $ KFun Star Star
 
-fn :: Type1 -> Type1 -> Type1
+fn :: Type -> Type -> Type
 fn a = TypeApp $ TypeApp tArrow a
 
-list :: Type1 -> Type1
+list :: Type -> Type
 list = TypeApp tList
 
-pair :: Type1 -> Type1 -> Type1
+pair :: Type -> Type -> Type
 pair = TypeApp . TypeApp tTuple2
 
 class HasKind t where
@@ -57,18 +55,18 @@ instance HasKind TVar where
 instance HasKind TCon where
   kind (TCon _ k) = k
 
-instance HasKind Type1 where
+instance HasKind Type where
   kind (TypeVar v) = kind v
   kind (TypeCon c) = kind c
   kind (TypeApp v _) = let (KFun _ k) = kind v in k
 
-type Subst = Map.Map TVar Type1
+type Subst = Map.Map TVar Type
 
 class Types t where
   apply :: Subst -> t -> t
   ftv :: t -> Set.Set TVar
 
-instance Types Type1 where
+instance Types Type where
   apply s v @ (TypeVar name) = Map.findWithDefault v name s
   apply s (TypeApp a b) = TypeApp (apply s a) (apply s b)
   apply _ t = t
@@ -101,7 +99,7 @@ merge = mergeA
 
 -- |
 -- Obtains the most general unifier on two types.
-mgu :: Monad m => Type1 -> Type1 -> m Subst
+mgu :: Monad m => Type -> Type -> m Subst
 mgu (TypeApp a b) (TypeApp c d) = do
   s <- mgu a c
   t <- mgu (apply s b) $ apply s d
@@ -112,7 +110,7 @@ mgu t (TypeVar u) = varBind u t
 mgu (TypeCon c) (TypeCon c') | c == c' = return Map.empty
 mgu _ _ = fail "types do not unify"
 
-varBind :: Monad m => TVar -> Type1 -> m Subst
+varBind :: Monad m => TVar -> Type -> m Subst
 varBind u t
   | t == TypeVar u = return Map.empty
   | u `Set.member` ftv t = fail "occur check fails"
@@ -122,7 +120,7 @@ varBind u t
 -- |
 -- Matching is like unification, but it finds a substitution @s@ where
 -- @apply s t1 = t2@.
-match :: Monad m => Type1 -> Type1 -> m Subst
+match :: Monad m => Type -> Type -> m Subst
 match (TypeApp a b) (TypeApp c d) = do
   s <- match a c
   t <- match b d
@@ -135,7 +133,7 @@ match _ _ = fail "types do not match"
 data Qual t = [Pred] :=> t
   deriving (Eq, Show)
 
-data Pred = IsIn String Type1
+data Pred = IsIn String Type
   deriving (Eq, Show)
 
 instance Types t => Types (Qual t) where
@@ -155,9 +153,9 @@ matchPred :: Pred -> Pred -> Maybe Subst
 matchPred = lift match
 
 -- |
--- Lifts a function on two 'Type1's to one on two 'Pred's and checks that
+-- Lifts a function on two 'Type's to one on two 'Pred's and checks that
 -- classes of 'Pred's are the same.
-lift :: Monad m => (Type1 -> Type1 -> m a) -> Pred -> Pred -> m a
+lift :: Monad m => (Type -> Type -> m a) -> Pred -> Pred -> m a
 lift m (IsIn i t) (IsIn j u)
   | i == j = m t u
   | otherwise = fail "classes differ"
@@ -167,7 +165,7 @@ type Inst = Qual Pred
 
 data ClassEnv = ClassEnv
   { classes :: Map.Map String Class
-  , defaults :: [Type1]
+  , defaults :: [Type]
   }
 
 -- | Gets superclasses of the given class name on an environment.
@@ -242,7 +240,7 @@ entail ce ps p = elem p `any` map (bySuper ce) ps || maybe False (all $ entail c
 inHnf :: Pred -> Bool
 inHnf (IsIn _ t) = hnf t
   where
-    hnf :: Type1 -> Bool
+    hnf :: Type -> Bool
     hnf (TypeVar _) = True
     hnf (TypeCon _) = False
     hnf (TypeApp t _) = hnf t
@@ -267,14 +265,14 @@ simplify ce = loop []
 reduce :: Monad m => ClassEnv -> [Pred] -> m [Pred]
 reduce ce ps = simplify ce <$> toHnfs ce ps
 
-data Scheme = Forall [Kind] (Qual Type1)
+data Scheme = Forall [Kind] (Qual Type)
   deriving Eq
 
 instance Types Scheme where
   apply s (Forall ks qt) = Forall ks $ apply s qt
   ftv (Forall _ qt) = ftv qt
 
-quantify :: [TVar] -> Qual Type1 -> Scheme
+quantify :: [TVar] -> Qual Type -> Scheme
 quantify vs qt = Forall ks $ apply s qt
   where
     vs' :: [TVar]
@@ -286,7 +284,7 @@ quantify vs qt = Forall ks $ apply s qt
     s :: Subst
     s = Map.fromList . zip vs' $ map TypeGen [0..]
 
-toScheme :: Type1 -> Scheme
+toScheme :: Type -> Scheme
 toScheme t = Forall [] ([] :=> t)
 
 data Assump = String :>: Scheme
@@ -310,7 +308,7 @@ runTI = flip evalState (Map.empty, 0)
 getSubst :: TI Subst
 getSubst = gets fst
 
-unify :: Type1 -> Type1 -> TI ()
+unify :: Type -> Type -> TI ()
 unify t1 t2 = do
   s <- getSubst
   u <- mgu (apply s t1) $ apply s t2
@@ -322,18 +320,18 @@ extSubst = modify . first . (@@)
 enumId :: Int -> String
 enumId n = "v" ++ show n
 
-newTVar :: Kind -> TI Type1
+newTVar :: Kind -> TI Type
 newTVar k = state $ \(s, n) -> (TypeVar $ TVar (enumId n) k, (s, n + 1))
 
-freshInst :: Scheme -> TI (Qual Type1)
+freshInst :: Scheme -> TI (Qual Type)
 freshInst (Forall ks qt) = do
   ts <- mapM newTVar ks
   return $ inst ts qt
 
 class Instantiate t where
-  inst :: [Type1] -> t -> t
+  inst :: [Type] -> t -> t
 
-instance Instantiate Type1 where
+instance Instantiate Type where
   inst ts (TypeApp a b) = TypeApp (inst ts a) $ inst ts b
   inst ts (TypeGen n) = ts !! n
   inst _ t = t
@@ -349,7 +347,7 @@ instance Instantiate Pred where
 
 type Infer e t = ClassEnv -> [Assump] -> e -> TI ([Pred], t)
 
-tiLit :: Literal -> TI ([Pred], Type1)
+tiLit :: Literal -> TI ([Pred], Type)
 tiLit (LitChar _) = return ([], tChar)
 tiLit (LitInt _) = do
   v <- newTVar Star
@@ -363,7 +361,7 @@ data Pat =
   | PLit Literal
   | PCon Assump [Pat]
 
-tiPat :: Pat -> TI ([Pred], [Assump], Type1)
+tiPat :: Pat -> TI ([Pred], [Assump], Type)
 tiPat (PVar i) = do
   v <- newTVar Star
   return ([], [i :>: toScheme v], v)
@@ -387,7 +385,7 @@ tiPat (PCon (_ :>: sc) pats) = do
   unify t (foldr fn t' ts)
   return (ps ++ qs, as, t')
 
-tiPats :: [Pat] -> TI ([Pred], [Assump], [Type1])
+tiPats :: [Pat] -> TI ([Pred], [Assump], [Type])
 tiPats pats = do
   triples <- mapM tiPat pats
   let ps = concatMap fst triples
@@ -399,7 +397,7 @@ tiPats pats = do
     snd (_, b, _) = b
     trd (_, _, c) = c
 
-tiExpr :: Infer Expr Type1
+tiExpr :: Infer Expr Type
 tiExpr _ as (Var i) = do
   (ps :=> t) <- find i as >>= freshInst
   return (ps, t)
@@ -413,7 +411,7 @@ tiExpr ce as (App e f) = do
 
 type Alt = ([Pat], Expr)
 
-tiAlt :: Infer Alt Type1
+tiAlt :: Infer Alt Type
 tiAlt ce as (pats, e) = do
   (ps, as', ts) <- tiPats pats
   (qs, t) <- tiExpr ce (as' ++ as) e
@@ -422,7 +420,7 @@ tiAlt ce as (pats, e) = do
 tiAlts :: ClassEnv -- ^ Class environment.
        -> [Assump] -- ^ Assumptions.
        -> [Alt]    -- ^ Alternatives.
-       -> Type1    -- ^ Expected type for the alternatives.
+       -> Type    -- ^ Expected type for the alternatives.
        -> TI [Pred]
 tiAlts ce as alts t = do
   double <- forM alts $ tiAlt ce as
@@ -444,7 +442,7 @@ ambiguities _ vs ps = [(v, filter (elem v . ftv) ps) | v <- Set.toList $ ftv ps 
 numClasses :: [String]
 numClasses = ["Num"]
 
-candidates :: ClassEnv -> Ambiguity -> [Type1]
+candidates :: ClassEnv -> Ambiguity -> [Type]
 candidates ce (v, qs) = [t' | all (TypeVar v ==) ts,
                               any (`elem` numClasses) is,
                               t' <- defaults ce,
@@ -453,10 +451,10 @@ candidates ce (v, qs) = [t' | all (TypeVar v ==) ts,
     is :: [String]
     is = [i | IsIn i _ <- qs]
 
-    ts :: [Type1]
+    ts :: [Type]
     ts = [t | IsIn _ t <- qs]
 
-withDefaults :: Monad m => (([Ambiguity], [Type1]) -> a) -> ClassEnv -> Set.Set TVar -> [Pred] -> m a
+withDefaults :: Monad m => (([Ambiguity], [Type]) -> a) -> ClassEnv -> Set.Set TVar -> [Pred] -> m a
 withDefaults f ce vs ps
   | any null tss = fail "cannot resolve ambiguity"
   | otherwise = return $ f (vps, map head tss)
@@ -464,7 +462,7 @@ withDefaults f ce vs ps
     vps :: [Ambiguity]
     vps = ambiguities ce vs ps
 
-    tss :: [[Type1]]
+    tss :: [[Type]]
     tss = map (candidates ce) vps
 
 defaultedPreds :: Monad m => ClassEnv -> Set.Set TVar -> [Pred] -> m [Pred]
@@ -516,16 +514,16 @@ tiImpls ce as bs = do
              then (ds ++ rs, zipWith (:>:) is $ scs1 rs s ts)
              else (ds, zipWith (:>:) is $ scs2 rs s ts)
   where
-    scs1 :: [Pred] -> Subst -> [Type1] -> [Scheme]
+    scs1 :: [Pred] -> Subst -> [Type] -> [Scheme]
     scs1 rs s ts = map (quantify (Set.toList $ getGenerics s ts Set.\\ ftv rs) . ([] :=>)) $ apply s ts
 
-    scs2 :: [Pred] -> Subst -> [Type1] -> [Scheme]
+    scs2 :: [Pred] -> Subst -> [Type] -> [Scheme]
     scs2 rs s ts = map (quantify (Set.toList $ getGenerics s ts) . (rs :=>)) $ apply s ts
 
     is :: [String]
     is = map fst bs
 
-    getAssumps :: [Type1] -> [Assump]
+    getAssumps :: [Type] -> [Assump]
     getAssumps ts = zipWith (:>:) is (map toScheme ts) ++ as
 
     getPreds :: Subst -> [[Pred]] -> [Pred]
@@ -534,10 +532,10 @@ tiImpls ce as bs = do
     getFixed :: Subst -> Set.Set TVar
     getFixed s = ftv $ apply s as
 
-    vssOf :: Subst -> [Type1] -> [Set.Set TVar]
+    vssOf :: Subst -> [Type] -> [Set.Set TVar]
     vssOf s = map ftv . apply s
 
-    getGenerics :: Subst -> [Type1] -> Set.Set TVar
+    getGenerics :: Subst -> [Type] -> Set.Set TVar
     getGenerics s ts = foldr1 Set.union (vssOf s ts) Set.\\ getFixed s
 
 type BindGroup = ([Expl], [[Impl]])
