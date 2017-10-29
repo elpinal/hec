86c50e9d3d2058df8144139d00ccca1e19181a8e
elpinal <elpinal@users.noreply.github.com>
2017-10-29 23:54:05 +0900

    Format tests

--- test/Refine/ParseSpec.hs
+++ test/Refine/ParseSpec.hs
@@ -138,7 +138,7 @@ spec = do
 
   describe "parseCase" $
     it "parses a case" $ do
-      parseWhole parseCase "case 1 of 1 -> 1" `shouldSatisfy` rightIs (Case (int 1) [(PLit (LitInt 1), int 1)])
+      parseWhole parseCase "case 1 of 1 -> 1"     `shouldSatisfy` rightIs (Case (int 1) [(PLit (LitInt 1), int 1)])
       parseWhole parseCase "case 1 + 2 of 1 -> 1" `shouldSatisfy` rightIs (Case (BinOp "+" (int 1) $ int 2) [(PLit (LitInt 1), int 1)])
-      parseWhole parseCase "case 1 of _ -> 1" `shouldSatisfy` rightIs (Case (int 1) [(PWildcard, int 1)])
-      parseWhole parseCase "case 1 of n -> n" `shouldSatisfy` rightIs (Case (int 1) [(PVar "n", (Var "n"))])
+      parseWhole parseCase "case 1 of _ -> 1"     `shouldSatisfy` rightIs (Case (int 1) [(PWildcard, int 1)])
+      parseWhole parseCase "case 1 of n -> n"     `shouldSatisfy` rightIs (Case (int 1) [(PVar "n", Var "n")])
