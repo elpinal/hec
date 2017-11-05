e2267dbf3f7b26431bf0045f80b6b8c6648a7c69
elpinal <elpinal@users.noreply.github.com>
2017-11-05 22:55:03 +0900

    Update test for parsePair

--- test/Refine/ParseSpec.hs
+++ test/Refine/ParseSpec.hs
@@ -278,5 +278,7 @@ spec = do
       parseWhole parseNewType "newtype A = B a"   `shouldSatisfy` rightIs (NewTypeDecl "A" "B" (TypeVar $ TVar "a" Star))
 
   describe "parsePair" $
-    it "parses a pair" $
-      parseWhole parsePair "(1,2)" `shouldSatisfy` rightIs (Pair (int 1) (int 2))
+    it "parses a pair" $ do
+      parseWhole parsePair "(1,2)"     `shouldSatisfy` rightIs (Pair (int 1) (int 2))
+      parseWhole parsePair "(1, 2)"    `shouldSatisfy` rightIs (Pair (int 1) (int 2))
+      parseWhole parsePair "( 1 , 2 )" `shouldSatisfy` rightIs (Pair (int 1) (int 2))
