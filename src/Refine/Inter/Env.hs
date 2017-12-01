cccb0a71b709ac9dc0efd48b664f2f8494248955
elpinal <elpinal@users.noreply.github.com>
2017-12-01 19:58:07 +0900

    Support scanning of TypeAnn

--- src/Refine/Inter/Env.hs
+++ src/Refine/Inter/Env.hs
@@ -12,7 +12,7 @@ import qualified Data.Map.Lazy as Map
 import qualified Refine.AST as AST
 import qualified Refine.Type.Syntactic as S
 
-type Vars  = Map.Map String (AST.Expr, Maybe S.Type)
+type Vars  = Map.Map String (Maybe AST.Expr, Maybe S.Type)
 type Types = Map.Map String S.Type
 
 data Decls = Decls
@@ -42,7 +42,15 @@ scanDecls :: [AST.Decl] -> Either DeclError Decls
 scanDecls ds = foldl (>=>) return (map scanDecl ds) emptyDecls
   where
     scanDecl :: AST.Decl -> Decls -> Either DeclError Decls
+
     scanDecl (AST.VarDecl i e) d =
-      if i `Map.member` vars d
-        then Left $ Duplicate i
-        else return $ updateVars (Map.insert i (e, Nothing)) d
+      case Map.lookup i $ vars d of
+        Just (Just e, t) -> Left $ Duplicate i
+        Just (Nothing, t) -> return $ updateVars (Map.insert i (Just e, t)) d
+        Nothing -> return $ updateVars (Map.insert i (Just e, Nothing)) d
+
+    scanDecl (AST.TypeAnn i t) d =
+      case Map.lookup i $ vars d of
+        Just (e, Just t) -> Left $ Duplicate i
+        Just (e, Nothing) -> return $ updateVars (Map.insert i (e, Just t)) d
+        Nothing -> return $ updateVars (Map.insert i (Nothing, Just t)) d
--- test/Refine/Inter/EnvSpec.hs
+++ test/Refine/Inter/EnvSpec.hs
@@ -6,14 +6,19 @@ import qualified Data.Map.Lazy as Map
 
 import Refine.AST
 import Refine.Inter.Env
+import qualified Refine.Type.Syntactic as S
 
 spec :: Spec
 spec = do
   describe "scanDecls" $ do
     it "scans declarations" $ do
       scanDecls []                     `shouldBe` return emptyDecls
-      scanDecls [VarDecl "x" $ int 23] `shouldBe` return (updateVars (Map.insert "x" (int 23, Nothing)) emptyDecls)
+      scanDecls [VarDecl "x" $ int 23] `shouldBe` return (updateVars (Map.insert "x" (Just $ int 23, Nothing)) emptyDecls)
+
+      scanDecls [TypeAnn "x" $ S.TypeCon "A"]                       `shouldBe` return (updateVars (Map.insert "x" (Nothing, Just $ S.TypeCon "A")) emptyDecls)
+      scanDecls [TypeAnn "x" $ S.TypeCon "A", VarDecl "x" $ int 23] `shouldBe` return (updateVars (Map.insert "x" (Just $ int 23, Just $ S.TypeCon "A")) emptyDecls)
 
     context "when given duplicated declarations" $
       it "return an error" $ do
-        scanDecls [VarDecl "x" $ int 23, VarDecl "x" $ int 0] `shouldBe` Left (Duplicate "x")
+        scanDecls [VarDecl "x" $ int 23, VarDecl "x" $ int 0]                `shouldBe` Left (Duplicate "x")
+        scanDecls [TypeAnn "x" $ S.TypeCon "A", TypeAnn "x" $ S.TypeVar "a"] `shouldBe` Left (Duplicate "x")
